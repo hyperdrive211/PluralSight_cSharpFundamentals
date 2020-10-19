fc34c1653ce0a826a867398dc7933a8939ed4e8a
Johnny <jmccauley117@gmail.com>
2020-10-19 21:16:21 +0100

    approached 4.6 in c# fundamentlas.
    In this section we examined state and, namespaces and the scope of namespaces.
    
    examined nulls and nullpointerexceptions. we begun a contructor method, examined the differeing betwee implicit and explicit constructors. we created an explicit constuctor.

---
+++ PluralSight_cSharpFundamentals/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PluralSight_cSharpFundamentals
+{
+    class Book
+    {
+
+        public Book() {
+            grades = new List<double>(); 
+        }
+     
+        public void AddGrade(double grade) {
+            grades.Add(grade); 
+        }
+    }
+}
--- PluralSight_cSharpFundamentals/Program.cs
+++ PluralSight_cSharpFundamentals/Program.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 
 namespace PluralSight_cSharpFundamentals
 {
+
+   
     class Program
     {
         static void Main(string[] args)
         {
-            writeName("Johnny", 34.1, 10.3);
-           
+
+            Book book = new Book(); 
         }
 
        
     }
+
+
 }
