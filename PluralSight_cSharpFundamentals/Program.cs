175444bf02f31645c4f14c7165cf8b4108e95cc4
Johnny <jmccauley117@gmail.com>
2020-10-19 20:48:01 +0100

    Add project files.

---
+++ PluralSight_cSharpFundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PluralSight_cSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            writeName("Johnny", 34.1, 10.3);
+           
+        }
+
+       
+    }
+}
