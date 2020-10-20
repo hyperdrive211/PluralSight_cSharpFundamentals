da7bae9ffbe181dd0dbe7852efb3ae6568a719df
Johnny <jmccauley117@gmail.com>
2020-10-20 16:34:21 +0100

    Add test folder

---
+++ PluralSight_cSharpFundatmentals_Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PluralSight_cSharpFundatmentals_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
