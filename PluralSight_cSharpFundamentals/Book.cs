a8b601e0c374cdbec6db9f6f1d4849416b360dc0
Johnny <jmccauley117@gmail.com>
2020-10-20 14:57:28 +0100

    complete module four.
    In this section the following was completed.
    
    A better undrsatadning on instantiation and the static key word. A better understanding of constructors and 'this' keyword.
    
    An example of methods and scope was perfromed by included and 'Show statistics method. '

--- PluralSight_cSharpFundamentals/Book.cs
+++ PluralSight_cSharpFundamentals/Book.cs
@@ -7,13 +7,33 @@ namespace PluralSight_cSharpFundamentals
 {
     class Book
     {
+        private List<double> grades;
+        private string name; 
 
-        public Book() {
+        public Book(string name) {
+            this.name = name; 
             grades = new List<double>(); 
         }
      
         public void AddGrade(double grade) {
-            grades.Add(grade); 
+            grades.Add(grade);
+        }
+
+        public void ShowStats() {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var num in grades) {
+                lowGrade = Math.Min(num, lowGrade);
+                highGrade = Math.Max(num, highGrade);
+                result += num;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}"); 
         }
     }
 }
--- PluralSight_cSharpFundamentals/Program.cs
+++ PluralSight_cSharpFundamentals/Program.cs
@@ -7,10 +7,15 @@ namespace PluralSight_cSharpFundamentals
    
     class Program
     {
+
         static void Main(string[] args)
         {
 
-            Book book = new Book(); 
+            Book book = new Book("Our Book Object");
+            book.AddGrade(30.0);
+            book.AddGrade(60.0);
+            book.AddGrade(77.5);
+            book.ShowStats(); 
         }
 
        
