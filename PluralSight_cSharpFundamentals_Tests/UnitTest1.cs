b5713bf37787a20122bfa96591787de135b63aae
Johnny <jmccauley117@gmail.com>
2020-10-20 18:16:41 +0100

    Updated Test file

---
+++ PluralSight_cSharpFundamentals_Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit; 
+
+namespace PluralSight_cSharpFundamentals_Tests
+{
+     public class UnitTest1
+    {
+        [Fact]
+        public void Test1() {
+            //arrange 
+            var x = 5;
+            var y = 2;
+            var expected = 7;
+
+            //act 
+            var actual = x + y;
+
+            //assert
+            Assert.Equal(expected, actual); 
+        }
+    }
+}
